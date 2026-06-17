55d25322a7305dda6160850e3d0db8026002c090
Luiz Mo <luizming07@gmail.com>
2026-06-17 20:29:56 -0300

    structs No e Fila_Prio

--- heap.h
+++ heap.h
@@ -1,6 +1,15 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+typedef struct{
+    int indice;
+    float erro; /*prioridade*/
+} No;
+
+typedef struct{
+    No *heap;
+    int tam;
+} Fila_Prio;
 
 void cria_heap();
 void insere_heap(int indice_ponto);
