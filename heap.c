f2574a27e5a99dbf46261e6f893d45bd4f1bd1c4
Luiz Mo <luizming07@gmail.com>
2026-06-19 00:40:07 -0300

    [add]insere_heap, extrai_min_heap e destroi_heap

--- heap.c
+++ heap.c
@@ -3,32 +3,83 @@
 #include "heap.h"
 #include "ponto.h"
 
+void sacode_heap(Heap *heap, int i){
+    No aux;
+    i *= 2;
+
+    while(i <= heap->tam){
+        if(i < heap->tam && heap->v[i].erro > heap->v[i+1].erro)
+            i++;
+
+        if(heap->v[i/2].erro <= heap->v[i].erro) /*e heap*/
+            break;
+
+        aux = heap->v[i/2];
+        heap->v[i/2] = heap->v[i];
+        heap->v[i] = aux;
+        i *= 2;
+    }
+}
+
 Heap *cria_heap(int n) {
     Heap *heap;
 
     if(!(heap = malloc(sizeof(Heap))))
         return NULL;
 
-    if(!(heap->v = malloc(sizeof(No) * n))){
+    if(!(heap->v = malloc(sizeof(No) * (n + 1)))){
         free(heap);
         heap = NULL;
 
         return NULL;
     }
 
+    heap->tam = 0;
+
     return heap;
 }
 
-void insere_heap(int indice_ponto) {
+void insere_heap(Heap *heap, int indice_ponto) {
+    No novo, aux;
 
+    novo.indice = indice_ponto;
+    novo.erro = pontos[indice_ponto].erro;
+    
+    heap->tam++;
+    heap->v[heap->tam] = novo; /*insere no final do heap*/
 
+    int i = heap->tam;
+    while(heap->v[i].erro < heap->v[i/2].erro){
+        aux = heap->v[i/2];
+        heap->v[i/2] = heap->v[i];
+        heap->v[i] = aux;
+        i /= 2;
+    }
 }
 
-int  extrai_min_heap() {
+int extrai_min_heap(Heap *heap) {
+    int removido = heap->v[1].indice;
 
+    if(heap->tam != 0)
+        heap->v[1] = heap->v[heap->tam];
 
+    heap->tam--;
+
+    sacode_heap(heap, 1);
+
+    return removido;
 }
 
 void atualiza_heap(int indice_ponto) {
 
+}
+
+Heap *destroi_heap(Heap *heap) {
+    free(heap->v);
+    heap->v = NULL;
+
+    free(heap);
+    heap = NULL;
+
+    return heap;
 }
--- heap.h
+++ heap.h
@@ -11,9 +11,14 @@ typedef struct{
     int tam;
 } Heap;
 
-Heap cria_heap(int n);
-void insere_heap(int indice_ponto);
-int  extrai_min_heap();
+Heap *cria_heap(int n);
+
+void insere_heap(Heap *heap, int indice_ponto);
+
+int extrai_min_heap(Heap *heap);
+
 void atualiza_heap(int indice_ponto);
+
+Heap *destroi_heap();
 
 #endif
